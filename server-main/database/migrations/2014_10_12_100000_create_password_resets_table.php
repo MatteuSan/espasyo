7e3153b5f8883f1b609b4c26468d2fde1658d7d6
Matteu <matteugt@gmail.com>
2021-12-23 11:14:02 +0800

    SERVER_MAIN: Update password resets table

--- server-main/database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ server-main/database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,19 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
-{
-    public function up()
-    {
+class CreatePasswordResetsTable extends Migration {
+
+    public function up() {
         Schema::create('password_resets', function (Blueprint $table) {
+            $table->id();
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }
 
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('password_resets');
     }
 }
